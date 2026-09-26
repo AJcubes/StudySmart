3a7a22c0f399bc6813f0f39ea68b810311557bc6
AJ-cubes <ajcubes33@gmail.com>
2026-09-26 13:10:12 +0800

    More CSS

--- src/js/script.js
+++ src/js/script.js
@@ -78,7 +78,7 @@ async function showDashboard() {
     auth.style.display = "none";
     dashboard.style.display = "block";
 
-    emailValue.innerText = `Email: ${currentUser}`;
+    emailValue.innerHTML = `<b>Email: </b>${currentUser}`;
     calendarURL.value = await cloudStorage.getItem(currentUser, "url") || "";
     receiveEmails.checked = await cloudStorage.getItem(currentUser, "receive_emails") === true;
 
